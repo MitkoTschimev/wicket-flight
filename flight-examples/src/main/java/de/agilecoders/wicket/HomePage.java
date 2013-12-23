8b924251b67ae70076adc504a24685041837d7fc
Michael Haitz <michael.haitz@gmx.com>
2013-12-24 00:14:35 +0100

    fix major maven problems

--- flight-examples/src/main/java/de/agilecoders/wicket/HomePage.java
+++ flight-examples/src/main/java/de/agilecoders/wicket/HomePage.java
@@ -1,20 +1,19 @@
 package de.agilecoders.wicket;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
+import de.agilecoders.wicket.flight.FlightBehavior;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public HomePage(final PageParameters parameters) {
-		super(parameters);
+    public HomePage(final PageParameters parameters) {
+        super(parameters);
 
-        Label label = new org.apache.wicket.markup.html.basic.Label();
+        Label label = new Label("behaviorTest");
+        label.add(new FlightBehavior("TestComponent"));
 
-        label.add(new de.agilecoders.wicket.flight.FlightBehavior("TestComponent"))
         add(label);
-		// TODO Add your page's components here
-
     }
 }
