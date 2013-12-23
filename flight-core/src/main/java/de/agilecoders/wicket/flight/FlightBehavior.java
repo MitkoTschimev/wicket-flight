932cddaaa718d0cf6e9facee881fbb2b3b30a1e9
Michael Haitz <michael.haitz@gmx.com>
2013-12-24 00:29:43 +0100

    minor improvements to FlightBehavior

--- flight-core/src/main/java/de/agilecoders/wicket/flight/FlightBehavior.java
+++ flight-core/src/main/java/de/agilecoders/wicket/flight/FlightBehavior.java
@@ -7,9 +7,10 @@ import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.request.resource.ResourceReference;
 import org.apache.wicket.util.lang.Args;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
 
 /**
@@ -17,17 +18,17 @@ import java.util.Map;
  */
 public class FlightBehavior extends Behavior {
 
-
+    private static final ResourceReference flightReference = new WicketFlightJavascriptReference();
     private final Map<String, IModel<String>> componentData;
     private final String flightComponentName;
 
     /**
-     * Construct.
+     * Construct, uses an empty custom component data map.
      *
      * @param flightComponentName the name of the flight component
      */
     public FlightBehavior(String flightComponentName) {
-        this(flightComponentName, new HashMap<String, IModel<String>>());
+        this(flightComponentName, Collections.<String, IModel<String>>emptyMap());
     }
 
     /**
@@ -46,39 +47,42 @@ public class FlightBehavior extends Behavior {
         super.onComponentTag(component, tag);
 
         tag.append("class", "js-flight-component", " ");
-        tag.put("data-component", checkIsCamelCase(flightComponentName));
 
-        for (String key : componentData.keySet()) {
-            tag.append("data-component-" + key, componentData.get(key).getObject(), " ");
+        if (component.getApplication().usesDevelopmentConfig()) {
+            assertCamelCase(flightComponentName);
+        }
+
+        tag.put("data-component", flightComponentName);
+
+        for (Map.Entry<String, IModel<String>> entry : componentData.entrySet()) {
+            tag.append("data-component-" + entry.getKey(), entry.getValue().getObject(), " ");
         }
     }
 
     /**
-     * flight component names are camelcase
+     * flight component names are camel-case
      *
      * @param flightComponentName flight component name
-     * @return the name valid
      */
-    private static String checkIsCamelCase(String flightComponentName) {
+    private static void assertCamelCase(String flightComponentName) {
         Args.notNull(flightComponentName, "flight component name");
         Args.isTrue(flightComponentName.charAt(0) == flightComponentName.toUpperCase().charAt(0), "flight component should be camelcase and start with an uppercase letter");
         Args.isTrue(StringUtils.isAlphanumeric(flightComponentName), "flight component should only contain alphanumeric character");
-        return flightComponentName;
     }
 
     @Override
     public void renderHead(Component component, IHeaderResponse response) {
         super.renderHead(component, response);
 
-        response.render(JavaScriptHeaderItem.forReference(new WicketFlightJavascriptReference()));
+        response.render(JavaScriptHeaderItem.forReference(flightReference));
     }
 
     @Override
     public void detach(Component component) {
         super.detach(component);
 
-        for (String key : componentData.keySet()) {
-            componentData.get(key).detach();
+        for (Map.Entry<String, IModel<String>> entry : componentData.entrySet()) {
+            entry.getValue().detach();
         }
     }
 }
