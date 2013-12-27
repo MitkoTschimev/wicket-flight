c012b07d33feb2a20db4da06e93d11f69f934925
Mitko Tschimev <mitko.tschimev@icloud.com>
2013-12-27 15:43:42 +0100

    removed css dependency because a flight component can be without styles css is not task of the flight behavior

--- flight-core/src/main/java/de/agilecoders/wicket/flight/SimpleFlightBehavior.java
+++ flight-core/src/main/java/de/agilecoders/wicket/flight/SimpleFlightBehavior.java
@@ -1,10 +1,8 @@
 package de.agilecoders.wicket.flight;
 
 import org.apache.wicket.Component;
-import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
-import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
 
 /**
@@ -25,8 +23,8 @@ public class SimpleFlightBehavior extends AbstractFlightBehavior {
 
     @Override
     protected void addComponentResourceReferences(Component component, IHeaderResponse response) {
-        String componentName = getComponentName(component);
-        response.render(JavaScriptHeaderItem.forReference(new JavaScriptResourceReference(component.getClass(), String.format("%s.js", componentName))));
-        response.render(CssHeaderItem.forReference(new CssResourceReference(component.getClass(), String.format("%s.css", componentName))));
+        response.render(JavaScriptHeaderItem.forReference(
+                new JavaScriptResourceReference(component.getClass(), String.format("%s.js", getComponentName(component)))
+        ));
     }
 }
