64be68901278629c51cba0be9656092cdd19df6b
Mitko Tschimev <mitko.tschimev@icloud.com>
2014-01-30 18:08:42 +0100

    removed inheritance from wicket flight

--- flight-core/src/main/java/de/agilecoders/wicket/flight/res/WicketFlight.js
+++ flight-core/src/main/java/de/agilecoders/wicket/flight/res/WicketFlight.js
@@ -29,47 +29,37 @@ defineComponent = (function (WicketFlightManager, flight) {
         });
     }
 
-    /*
-     * A mixin to add namespace functionality
+    /**
+     * Add our wicket flight custom core mixins
+     *
+     * @param mixins
      */
-    function namespaceMixin() {
-        /**
-         * Returns an event name namespaced to the component.
-         *
-         * E.g. "click" -> "click.componentX"
-         *
-         * @param {String} eventName
-         * @return {String} The namespaced event name
-         */
-        this.getNameSpacedEventName = function (eventName) {
-            return eventName + "." + this.name;
-        };
+    function addWicketFlightCoreMixins(mixins) {
+        mixins.unshift(teardownMixin);
+    }
+
+    /**
+     * Creates a new flight component with the registered mixins
+     *
+     * @param mixins
+     * @returns {FlightComponent}
+     */
+    function createFlightComponent(mixins) {
+        return flight.component.apply(null, mixins);
     }
 
     /**
      * Wrapper to define a component and add all logic for integration
      * with Wicket.
      *
-     * @param options.name {string} The components name as used in its
-     *          data-fc attribute
-     * @param options.mixins {Array} An array of mixin functions as used in
-     *          flight
-     * @param options.parent {FlightComponent} optional A component to
-     *          inherit from.
+     * @param options.name {string} The components name as used in its data-fc attribute
+     * @param options.mixins {Array} An array of mixin functions as used in flight
      * @return {FlightComponent} A new Flight component
      */
     function defineComponent(options) {
         var mixins = options.mixins,
             name = options.name,
-            parent = options.parent,
-            parentMixins, component;
-
-        /**
-         * Add the component name to the component instances.
-         */
-        function nameMixin() {
-            this.name = name;
-        }
+            component;
 
         // 'Be liberal in what you accept', Postel's law
         // If mixins is not an array but a function, the caller most likely
@@ -78,31 +68,8 @@ defineComponent = (function (WicketFlightManager, flight) {
             mixins = [mixins];
         }
 
-        if (parent) {
-            // If there is a parent component to inherit from, extract all
-            // its mixins (minus the base mixins from flight itself) and
-            // concatenate the mixins for this new component.
-            // This is the critical part, because it relies on flight's
-            // internal implementation. However, there is no other way. At
-            // least we have this part at a single location and not spread
-            // amongst all components.
-            parentMixins = parent.prototype.mixedIn.slice(3);
-
-            mixins = parentMixins.concat(mixins);
-        } else {
-            // If there is no parent, we need to add the teardown mixin
-            mixins.unshift(teardownMixin);
-            // If there is no parent, we need to add the namespace mixin
-            mixins.unshift(namespaceMixin);
-        }
-
-        // To set the right component name, the name mixin must come last. This
-        // is because the parent has its own nameMixin, which we need to
-        // overwrite. Of course we could remove the parent's nameMixin, still
-        // this would have to come last.
-        mixins.push(nameMixin);
-
-        component = flight.component.apply(null, mixins);
+        addWicketFlightCoreMixins(mixins);
+        component = createFlightComponent(mixins);
 
         if (name) {
             // Register the component at the WicketFlightManager
